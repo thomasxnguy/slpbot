1449a1180a1327d3cc89ed8cf65e5bf11a3ed3e3
Thomas NGUY <thomasxnguy@gmail.com>
2020-06-21 19:13:32 +0900

    change deposit help usage

--- src/commands/deposit.ts
+++ src/commands/deposit.ts
@@ -1,6 +1,4 @@
 import { CommandHandler } from '../types';
-import {isNumber, truncateDecimals} from "../utils";
-import {withdrawalFund} from "../services/withdrawal.service";
 
 const Description = 'Deposit tokens'
 
@@ -15,28 +13,11 @@ const Handler: CommandHandler = async ctx => {
     return;
   }
 
-  const usage = () =>
-    ctx.reply(
-      `Usage: /deposit <txId>. Example: /deposit f1aa8276f055fc0497ed5c5dc5fe2b8626fa1dacef9d06a5bc1de3941bb5e3b6`
-    );
+  const { account } = ctx;
 
-  const { args, account } = ctx;
-
-  if (args.length !== 1) {
-    await usage();
-    return;
-  }
-
-  const [txId] = args;
-
-  try {
-    // Do deposit
-  } catch (error) {
-      await ctx.reply('Deposit failed, please try again');
-      return;
-  }
-
-  await ctx.reply(`Your deposit has been registered, I will contact you again when it will be confirmed!`);
+  await ctx.reply(
+      `Usage: Send deposit to this address: @${account.slpAddress}`
+  );
 }
 
 
